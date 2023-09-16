77eb23c66a50d0de783742d15fead57c0c6f034f
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-09-16 00:52:13 -0700

    working

---  working_on_the_first/lib/main.dart
+++  working_on_the_first/lib/main.dart
@@ -1,37 +1,51 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 import 'package:working_on_the_first/home_page.dart';
 
-
-void main(){
+void main() {
   runApp(MyApp());
-}           
-
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(), // Use the HomePage widget here
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.green),
+      routes: {
+        '/MyCenteredMessage': (context) => HomePage(),
+        '/': (context) => MyCenteredMessage()
+      },
     );
   }
 }
-
 
 class MyCenteredMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Centered Message'),
-      ),
-      body: Center(
-        child: Text(
-          'This is the first  of application',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: Text('image shower '),
         ),
-      ),
-    );
+        body: Column(children: [
+          Container(
+            width: 150.0, // Set the desired width
+            height: 200.0, // Set the desired height
+            child: Image.asset('assets/images/loginpage.png'),
+          ),
+          SizedBox(
+            height: 50, // <-- SEE HERE
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter something',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ]));
   }
 }
