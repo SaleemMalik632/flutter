ee98c2d0328c7313b4c129419da61b1e95cc6b62
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-09-20 08:50:43 -0700

    working

---  working_on_the_first/lib/main.dart
+++  working_on_the_first/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace, must_be_immutable
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace, must_be_immutable, sort_child_properties_last, non_constant_identifier_names, use_build_context_synchronously
 import 'package:flutter/material.dart';
 import 'package:working_on_the_first/home_page.dart';
 
@@ -26,53 +26,88 @@ class MyCenteredMessage extends StatefulWidget {
 }
 
 class _MyCenteredMessageState extends State<MyCenteredMessage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String name = '';
+  bool Check_click = false;
+
+  Move_on(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        Check_click = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => TextPage()),
+      );
+      setState(() {
+        Check_click = false;
+      });
+    } 
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login Page')),
-      body: Column(
-        children: [
-          Container(
-            width: 150.0, // Set the desired width
-            height: 200.0, // Set the desired height
-            child: Image.asset('assets/images/loginpage.png'),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Wellcome Page $name',
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(height: 10),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: TextField(
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              width: 150.0,
+              height: 200.0,
+              child: Image.asset('assets/images/loginpage.png'),
+            ),
+            Text('Welcome Page', style: TextStyle(fontSize: 20)),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value == '') {
+                  return 'Please Enter Name';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 labelText: 'Name',
                 border: OutlineInputBorder(),
               ),
             ),
-          ),
-          SizedBox(height: 10),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: TextField(
+            SizedBox(height: 10),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value == '') {
+                  return 'Email Addesss';
+                }
+                return null;
+              },
               decoration: InputDecoration(
-                  labelText: 'Email', border: OutlineInputBorder()),
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => TextPage()),
-              );
-            },
-            child: Text('Sign In'), // Replace with your button text
-          ),
-        ],
+            SizedBox(height: 5),
+            InkWell(
+              onTap: () => Move_on(context),
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                height: 30,
+                width: Check_click ? 50 : 100,
+                alignment: Alignment.center,
+                child: Check_click? Icon(Icons.done)
+                : Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(Check_click ? 50 : 10),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
