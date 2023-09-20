c459aa09a999c6ecc3fdf450a754519e0f9f406b
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-09-20 10:20:22 -0700

    working on flutter

---  working_on_the_first/lib/main.dart
+++  working_on_the_first/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace, must_be_immutable, sort_child_properties_last, non_constant_identifier_names, use_build_context_synchronously
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace, must_be_immutable, sort_child_properties_last, non_constant_identifier_names, use_build_context_synchronously, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 import 'package:working_on_the_first/home_page.dart';
 
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.green),
+      debugShowCheckedModeBanner: false,
       routes: {
         '/MyCenteredMessage': (context) => TextPage(),
         '/': (context) => MyCenteredMessage()
@@ -42,46 +43,54 @@ class _MyCenteredMessageState extends State<MyCenteredMessage> {
       setState(() {
         Check_click = false;
       });
-    } 
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login Page')),
+      drawer: MYDraw(),
       body: Form(
         key: _formKey,
         child: Column(
           children: [
             Container(
-              width: 150.0,
-              height: 200.0,
+              width: 150,
+              height: 200,
               child: Image.asset('assets/images/loginpage.png'),
             ),
             Text('Welcome Page', style: TextStyle(fontSize: 20)),
-            TextFormField(
-              validator: (value) {
-                if (value == null || value == '') {
-                  return 'Please Enter Name';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value == '') {
+                    return 'Please Enter Name';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
             SizedBox(height: 10),
-            TextFormField(
-              validator: (value) {
-                if (value == null || value == '') {
-                  return 'Email Addesss';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0), // Adjust the horizontal padding as needed
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value == '') {
+                    return 'Email Address';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
             SizedBox(height: 5),
@@ -92,8 +101,9 @@ class _MyCenteredMessageState extends State<MyCenteredMessage> {
                 height: 30,
                 width: Check_click ? 50 : 100,
                 alignment: Alignment.center,
-                child: Check_click? Icon(Icons.done)
-                : Text(
+                child: Check_click
+                    ? Icon(Icons.done)
+                    : Text(
                         'Login',
                         style: TextStyle(
                           color: Colors.white,
@@ -108,6 +118,88 @@ class _MyCenteredMessageState extends State<MyCenteredMessage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Column(children: [
+        SizedBox(height: 50),
+        Text(
+          'home Page ',
+          style: TextStyle(fontSize: 50),
+        )
+      ]),
+    );
+  }
+}
+
+class Services extends StatelessWidget {
+  const Services({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Services Page')),
+      body: Column(children: [
+        SizedBox(height: 50),
+        Text(
+          'Services Page ',
+          style: TextStyle(fontSize: 50),
+        )
+      ]),
+    );
+  }
+}
+
+
+
+
+class MYDraw extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+              accountName: Text('Saleem Malik',
+                  style: TextStyle(fontSize: 21, color: Colors.white)),
+              accountEmail: Text(
+                  'saleemalik444@gmail.com'), // You can add the user's email here if needed
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage(
+                    'assets/images/saleem.jpeg'), // Replace with the path to the user's image
+              )),
+          ListTile(
+            leading: Icon(
+              Icons.home, 
+            ),
+            title: const Text('Home'),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Home()));
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.business_center,
+            ),
+            title: const Text('Services'),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Services()));
+            },
+          ),
+        ],
       ),
     );
   }
